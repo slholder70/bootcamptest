742994ee0dca0799b199715de4c44f3ff75d4632
Six Four <slholder70@gmail.com>
2018-05-19 14:01:08 -0400

    test add

---
+++ Pies.py
@@ -0,0 +1,28 @@
+
+#pies list
+piesList = ["Pecan","Apple Crisp", "Bean", "Banoffee", "Black Bun", "Blueberry", "Buko", "Burek",  "Tamale", "Steak"]
+
+print ("Welcome to the House of Pies! Here are our pies:")
+
+# Print all of the pies to the screen and their index in brackets
+for pies in piesList:
+    print("[" + str(piesList.index(pies)) + "] " + pies)
+
+
+
+# Run through a loop which allows the user to choose which pies to take home with them
+for x in range(favorite):
+    selected = input("Which pie would you like to order? ")
+
+Immediately after, follow the order with Great! We'll have that <PIE NAME> right out for you. and then ask if they would like to make another order. If so, repeat the process.
+
+
+
+# A While Loop will continue to loop through the code until all choices are selected
+x = "Yes"
+while x == "Yes":
+    print("Whee! Merry-Go-Rounds are great!")
+    x = input("Would you like to go on the Merry-Go-Round again? ")
+
+
+#come back to this code and finish tonight for understanding
